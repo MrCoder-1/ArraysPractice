6c42b842a2f276b73bcb1bc1facffc833aacc1d9
Priyanshu Pathak <pathak0308@gmail.com>
2023-07-01 23:23:21 +0530

    initialization of the variables

--- src/JumpGame.java
+++ src/JumpGame.java
@@ -1,6 +1,13 @@
 public class JumpGame {
 
+    public  int NoOfJumps(int [] array){
+        int a = array[0];
+        int b = array[0];
+        int jumps = 1;
+    }
+
     public static void main(String[] args){
-        System.out.println("");
+        System.out.println("Hello World");
+        int nums[] = new int[]{1,3,5,8,9,2,6,7,6,8,9};
     }
 }
