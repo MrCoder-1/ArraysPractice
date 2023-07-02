5a0d9c87d9d21394ce847286ec10986108e2e888
Priyanshu Pathak <pathak0308@gmail.com>
2023-07-02 23:47:13 +0530

    check which set of consecutive numbers have previous smaller number

--- src/com/company/NextGreaterNumber.java
+++ src/com/company/NextGreaterNumber.java
@@ -3,7 +3,14 @@ package com.company;
 public class NextGreaterNumber {
 
     static void nextGreaterNumber(int[] arr){
-
+        int min = 0;
+        for (int i = arr.length-1; i > 1; i--) {
+            if(arr[i] < arr[i-1]){
+                min = i;
+                break;
+            }
+        }
+        
     }
 
     public static void main(String[] args) {
