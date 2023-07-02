a665361db641ea175e5a51d9de3af5c4ddf23acf
Priyanshu Pathak <pathak0308@gmail.com>
2023-07-02 23:23:41 +0530

    Intial commit

---
+++ src/com/company/NextGreaterNumber.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class NextGreaterNumber {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int [] arr = new int[]{2,1,8,7,6,5};
+    }
+}
