cab993ea93e92857e87279bf7c5fef02fb2bbc1e
Priyanshu Pathak <pathak0308@gmail.com>
2023-07-04 00:34:30 +0530

    function to move the zeroes to the end of the array

--- src/com/company/MoveZerosToEnd.java
+++ src/com/company/MoveZerosToEnd.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class MoveZerosToEnd {
+
+    static void moveZeroesToEnd(int [] array){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int[] array = new int[]{1,3,0,0,4,0,9};
+    }
 }
