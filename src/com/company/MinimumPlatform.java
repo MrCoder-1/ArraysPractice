709d18a315592892a024ae5b5e2491e8fc3637bb
Priyanshu Pathak <pathak0308@gmail.com>
2023-07-04 18:43:34 +0530

    function to find the minimum no of platform so that no train wait

--- src/com/company/MinimumPlatform.java
+++ src/com/company/MinimumPlatform.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class MinimumPlatform {
 
+    static void minimumPlatform(int[] arrival, int[] departure){
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int arrival[] = new int[]{100,140,150,200,215,400};
+        int departure[] = new int[]{110,220,230,300,315,600};
+        minimumPlatform(arrival,departure);
     }
 }
