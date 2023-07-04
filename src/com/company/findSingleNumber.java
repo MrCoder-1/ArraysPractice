b0e733e93a3c44d1037b14ff0670bff3bdbeabea
Priyanshu Pathak <pathak0308@gmail.com>
2023-07-04 23:39:54 +0530

    function to find the different number in the array

---
+++ src/com/company/findSingleNumber.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class findSingleNumber {
+
+    static void main(int [] array){
+        int length = array.length;
+        
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int[] array = new int[]{6,2,4,3,4,2,3};
+    }
+}
